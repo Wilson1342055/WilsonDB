a3b8ad46aed2f9d47aef8084aedb752cb10872ec
wilson <linweifeng@aukeys.com>
2020-09-18 15:23:21 +0800

    FirstCommit

---
+++ DapperHelper.cs
@@ -0,0 +1,392 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.Common;
+
+namespace BestWoDP
+{
+    public class DapperHelper
+    {
+
+
+        /// <summary>
+        /// 执行存储过程返回DataSet
+        /// </summary>
+        /// <param name="SPName"></param>
+        /// <param name="parameters"></param>
+        /// <param name="DBSetName"></param>
+        /// <param name="DB"></param>
+        /// <returns></returns>
+        public static DataSet RunProcedure_DataSet(string SPName, IDataParameter[] parameters, string DBSetName, DBConnection DB)
+        {
+            return RunProcedure_DataSet(SPName, parameters, DB);
+        }
+
+        /// <summary>
+        /// 执行存储过程返回DataSet
+        /// </summary>
+        /// <param name="SPName"></param>
+        /// <param name="parameters"></param>
+        /// <param name="sqlconnection"></param>
+        /// <returns></returns>
+        public static DataSet RunProcedure_DataSet(string SPName, IDataParameter[] parameters, DBConnection DB)
+        {
+            DynamicParameters pars = new DynamicParameters();
+            foreach (SqlParameter item in parameters)
+            {
+                pars.Add(item.ParameterName, item.Value, item.DbType, item.Direction, item.Size);
+            }
+            string StrConnection = GetConnectionStr(DB);
+            using (IDbConnection DBcon = new SqlConnection(StrConnection))
+            {
+                DataSet ds = new XDataSet();
+                var result = DBcon.ExecuteReader(SPName, pars, commandTimeout: 600, commandType: CommandType.StoredProcedure);
+                try
+                {
+                    ds.Load(result, LoadOption.OverwriteChanges, null, new DataTable[] { });
+                }
+                finally { result.Close(); }
+
+                return InitDS(ds);
+
+            }
+        }
+
+
+
+        /// <summary>
+        /// 执行存储过程返回DataTable
+        /// </summary>
+        /// <param name="SPName"></param>
+        /// <param name="parameters"></param>
+        /// <param name="sqlconnection"></param>
+        /// <returns></returns>
+        public static DataTable RunProcedure_DataTable(string SPName, IDataParameter[] parameters, DBConnection DB)
+        {
+            DynamicParameters pars = new DynamicParameters();
+            foreach (SqlParameter item in parameters)
+            {
+                pars.Add(item.ParameterName, item.Value, item.DbType, item.Direction, item.Size);
+            }
+            string StrConnection = GetConnectionStr(DB);
+            using (IDbConnection DBcon = new SqlConnection(StrConnection))
+            {
+                DataTable dt = new DataTable();
+                var result = DBcon.ExecuteReader(SPName, pars, commandTimeout: 600, commandType: CommandType.StoredProcedure);
+                try
+                {
+                    dt.Load(result);
+                }
+                finally { result.Close(); }
+
+                foreach (SqlParameter item in parameters)
+                {
+                    if (item.Direction == ParameterDirection.Output)
+                    {
+                        item.Value = pars.Get<object>(item.ParameterName);
+                    }
+                }
+                return InitDT(dt);
+            }
+        }
+
+        /// <summary>
+        /// 执行SP 返回SP返回值
+        /// </summary>
+        /// <param name="SPName"></param>
+        /// <param name="parameters"></param>
+        /// <param name="sqlconnection"></param>
+        /// <returns></returns>
+        public static int RunProcedure_Return(string SPName, IDataParameter[] parameters, DBConnection DB)
+        {
+            int AffectedCount = 0;
+            int intResult = RunProcedure_Return(SPName, parameters, DB, out AffectedCount);
+            return intResult;
+        }
+
+        /// <summary>
+        /// 执行SP 返回SP返回值及受影响的行数
+        /// </summary>
+        /// <param name="SPName"></param>
+        /// <param name="parameters"></param>
+        /// <param name="sqlconnection"></param>
+        /// <param name="AffectedCount"></param>
+        /// <returns></returns>
+        public static int RunProcedure_Return(string SPName, IDataParameter[] parameters, DBConnection DB, out int AffectedCount)
+        {
+            DynamicParameters pars = new DynamicParameters();
+            foreach (SqlParameter item in parameters)
+            {
+                pars.Add(item.ParameterName, item.Value, item.DbType, item.Direction, item.Size);
+            }
+
+            pars.Add("ReturnValue", null, DbType.Int32, ParameterDirection.ReturnValue, null);
+            string StrConnection = GetConnectionStr(DB);
+            using (IDbConnection DBcon = new SqlConnection(StrConnection))
+            {
+                int result = DBcon.Execute(SPName, pars, commandTimeout: 600, commandType: CommandType.StoredProcedure);
+                foreach (SqlParameter item in parameters)
+                {
+                    if (item.Direction == ParameterDirection.Output)
+                    {
+                        item.Value = pars.Get<object>(item.ParameterName) == null ? "" : pars.Get<object>(item.ParameterName);
+                    }
+                }
+
+                AffectedCount = result;
+
+                return pars.Get<int>("ReturnValue");
+            }
+        }
+
+
+        /// <summary>
+        /// 查询返回DataTable
+        /// </summary>
+        /// <param name="TableName"></param>
+        /// <param name="FieldString"></param>
+        /// <param name="WhereString"></param>
+        /// <returns></returns>
+        public static DataTable QueryGetDT(string TableName, string FieldString, string WhereString, DBConnection DB)
+        {
+            DataTable dt = new DataTable();
+            string StrConnection = GetConnectionStr(DB);
+            if (!WhereString.ToLower().Trim().StartsWith("and"))
+            {
+                WhereString = " and " + WhereString;
+            }
+            using (IDbConnection DBcon = new SqlConnection(StrConnection))
+            {
+                string sql = "select " + FieldString + " from " + TableName + " where 1=1 " + WhereString;
+                var result = DBcon.ExecuteReader(sql, null, commandTimeout: 600);
+                try
+                {
+                    dt.Load(result);
+                }
+                finally { result.Close(); }
+                return InitDT(dt);
+            }
+        }
+
+
+        /// <summary>
+        /// 更新记录
+        /// </summary>
+        /// <param name="TableName">表名</param>
+        /// <param name="UpdateString">更新的列和值</param>
+        /// <param name="WhereString">条件</param>
+        /// <param name="Parameters">参数对象</param>
+        /// <returns></returns>
+        public static int Update(string TableName, string UpdateString, string WhereString, DBConnection DB)
+        {
+            string StrConnection = GetConnectionStr(DB);
+            if (!WhereString.ToLower().Trim().StartsWith("and"))
+            {
+                WhereString = " and " + WhereString;
+            }
+            if (WhereString.Trim() == "")
+            {
+                return 0;
+            }
+            else
+            {
+                using (IDbConnection DBcon = new SqlConnection(StrConnection))
+                {
+                    string sql = "update " + TableName + " set " + UpdateString + " where 1=1 " + WhereString;
+                    var result = DBcon.Execute(sql, null, commandTimeout: 600);
+                    return result;
+                }
+            }
+        }
+        /// <summary>
+        /// 执行语句
+        /// </summary>
+        /// <param name="strSQL"></param>
+        /// <param name="DB"></param>
+        /// <returns></returns>
+        public static int ExceSQL(string strSQL, DBConnection DB)
+        {
+            string StrConnection = GetConnectionStr(DB);
+            using (IDbConnection DBcon = new SqlConnection(StrConnection))
+            {
+                var result = DBcon.Execute(strSQL, null, commandTimeout: 600);
+                return result;
+            }
+        }
+
+        /// <summary>
+        /// 执行新增语句返回ID
+        /// </summary>
+        /// <param name="strSQL"></param>
+        /// <param name="DB"></param>
+        /// <returns></returns>
+        public static int ExceSQLReturnID(string strSQL, DBConnection DB)
+        {
+            strSQL += ";SELECT @id=SCOPE_IDENTITY()";
+            string StrConnection = GetConnectionStr(DB);
+            using (IDbConnection DBcon = new SqlConnection(StrConnection))
+            {
+                var p = new DynamicParameters();
+                p.Add("@ID", dbType: DbType.Int32, direction: ParameterDirection.Output);
+                var affectCount = DBcon.Execute(strSQL, p);
+                return p.Get<int>("@ID");
+            }
+        }
+
+        public static T GetModel<T>(string TableName, string FieldString, string WhereString, DBConnection DB)
+        {
+            string StrConnection = GetConnectionStr(DB);
+            using (IDbConnection DBcon = new SqlConnection(StrConnection))
+            {
+                string sql = "select top 1 " + FieldString + " from " + TableName + " where 1=1 " + WhereString;
+                var result = DBcon.Query<T>(sql, null).FirstOrDefault();
+                return result;
+            }
+        }
+
+
+
+        public class XDataSet : DataSet
+        {
+            public override void Load(IDataReader reader, LoadOption loadOption, FillErrorEventHandler handler, params DataTable[] tables)
+            {
+                XLoadAdapter adapter = new XLoadAdapter
+                {
+                    FillLoadOption = loadOption,
+                    MissingSchemaAction = MissingSchemaAction.AddWithKey
+                };
+                if (handler != null)
+                {
+                    adapter.FillError += handler;
+                }
+                adapter.FillFromReader(this, reader, 0, 0);
+                if (!reader.IsClosed && !reader.NextResult())
+                {
+                    reader.Close();
+                }
+            }
+        }
+
+        public class XLoadAdapter : DataAdapter
+        {
+            public XLoadAdapter()
+            {
+            }
+
+            public int FillFromReader(DataSet ds, IDataReader dataReader, int startRecord, int maxRecords)
+            {
+                return this.Fill(ds, "Table", dataReader, startRecord, maxRecords);
+            }
+        }
+
+
+        private static DataSet InitDS(DataSet ds)
+        {
+            foreach (DataTable dt in ds.Tables)
+            {
+                foreach (DataColumn item in dt.Columns)
+                {
+                    item.ReadOnly = false;
+                }
+            }
+
+            return ds;
+        }
+
+        private static DataTable InitDT(DataTable dt)
+        {
+            foreach (DataColumn item in dt.Columns)
+            {
+                item.ReadOnly = false;
+            }
+            return dt;
+        }
+
+        public enum DBConnection
+        {
+            SystemHelper = 1,
+            TMSSplitHelper = 2,
+            OrderHelper=3,
+            LogHelper=4,
+        }
+        /// <summary>
+        /// 开发
+        /// </summary>
+        /// <param name = "DB" ></ param >
+        /// < returns ></ returns >
+        private static string GetConnectionStr(DBConnection DB)
+        {
+            string strConnection = "";
+            int intDBValue = (int)DB;
+            switch (intDBValue)
+            {
+                case 1:
+                    strConnection = "Data Source=120.77.233.164;Initial Catalog=SystemHelper;User Id=tms_dev;Password=tms_dev;";
+                    break;
+                case 2:
+                    strConnection = "Data Source=120.77.233.164;Initial Catalog=TMSSplitHelper;User Id=tms_dev;Password=tms_dev;";
+                    break;
+                //case 3:
+                //    strConnection = "Data Source=10.1.1.123;Initial Catalog=CommonHelper;User Id=WMSAdmin;Password=fdsoerjcmqwo2@a.23s!afds;";
+                //    break;
+                case 4:
+                    strConnection = "Data Source=120.77.233.164;Initial Catalog=LogHelper;User Id=tms_dev;Password=tms_dev;";
+                    break;
+                    //case 5:
+                    //    strConnection = "Data Source=10.1.1.123;Initial Catalog=LogHelper;User Id=WMSAdmin;Password=fdsoerjcmqwo2@a.23s!afds;";
+                    //    break;
+                    //case 6:
+                    //    strConnection = "Data Source=10.1.1.123;Initial Catalog=OrderHelper;User Id=WMSAdmin;Password=fdsoerjcmqwo2@a.23s!afds;";
+                    //    break;
+                    //case 7:
+                    //    strConnection = "Data Source=10.1.1.123;Initial Catalog=WareHouseHelper;User Id=WMSAdmin;Password=fdsoerjcmqwo2@a.23s!afds;";
+                    //    break;
+            }
+            return strConnection;
+        }
+
+        /// <summary>
+        /// 生产
+        /// </summary>
+        /// <param name="DB"></param>
+        /// <returns></returns>
+        //private static string GetConnectionStr(DBConnection DB)
+        //{
+        //    string strConnection = "";
+        //    int intDBValue = (int)DB;
+        //    switch (intDBValue)
+        //    {
+        //        case 1:
+        //            strConnection = "Data Source=db228.bestwo.net,19433;Initial Catalog=OrderHelper;User Id=aukeys_tms;Password=;";
+        //            break;
+        //        case 2:
+        //            strConnection = "Data Source=db228.bestwo.net,19433;Initial Catalog=BestWoStock;User Id=aukeys_tms;Password=;";
+        //            break;
+        //        case 3:
+        //            strConnection = "Data Source=db228.bestwo.net,19433;Initial Catalog=CommonHelper;User Id=aukeys_tms;Password=;";
+        //            break;
+        //        case 4:
+        //            strConnection = "Data Source=db228.bestwo.net,19433;Initial Catalog=ErpHelper;User Id=aukeys_tms;Password=;";
+        //            break;
+        //        case 5:
+        //            strConnection = "Data Source=db228.bestwo.net,19433;Initial Catalog=LogHelper;User Id=aukeys_tms;Password=;";
+        //            break;
+        //        case 6:
+        //            strConnection = "Data Source=db228.bestwo.net,19433;Initial Catalog=OrderHelper;User Id=aukeys_tms;Password=;";
+        //            break;
+        //        case 7:
+        //            strConnection = "Data Source=db228.bestwo.net,19433;Initial Catalog=WareHouseHelper;User Id=aukeys_tms;Password=;";
+        //            break;
+        //    }
+        //    return strConnection;
+        //}
+
+
+
+
+    }
+}
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,147 @@
+﻿using BestWoDP;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            DataSet ds = null;
+            try
+            {
+                IDataParameter[] parameters ={ 
+                                            new SqlParameter("@orgId",SqlDbType.Int),
+                                            new SqlParameter("@customerOrderIds",SqlDbType.NVarChar,5000),
+                                            new SqlParameter("@PageIndex",SqlDbType.Int),
+                                            new SqlParameter("@PageSize",SqlDbType.Int),
+                                            new SqlParameter("@StartTime",SqlDbType.NVarChar,50),
+                                            new SqlParameter("@EndTime",SqlDbType.NVarChar,50),
+                                         };
+                parameters[0].Value = 35;
+                parameters[1].Value = "AK20033100013";
+                parameters[2].Value = 1;
+                parameters[3].Value = 200;
+                parameters[4].Value = "";
+                parameters[5].Value = "";
+
+
+                ds = DapperHelper.RunProcedure_DataSet("[UP_OrderAndPackage_Qurey]", parameters,DapperHelper.DBConnection.OrderHelper);
+
+                //ds = BaseBll.RunProcedure_DataSet("[UP_OrderAndPackage_Qurey]", parameters, "TmepOrderInfo", Aukeys.Frame.Dao.Util.DbConnection.getOrderSConnection());
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            MessageBox.Show(ds.Tables.Count.ToString());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                IDataParameter[] parameters = { 
+                                            new SqlParameter("@OrgId",SqlDbType.NVarChar,10),
+                                            new SqlParameter("@StoreHouseId", SqlDbType.Int), 
+                                            new SqlParameter("@StartTime", SqlDbType.NVarChar,50 ), 
+                                            new SqlParameter("@EndTime", SqlDbType.NVarChar,50 ), 
+                                            new SqlParameter("@PageIndex",SqlDbType.Int),
+                                            new SqlParameter("@PageSize",SqlDbType.Int),
+                                            new SqlParameter("@PageCount",SqlDbType.Int ), //页码总数
+                                            new SqlParameter("@RecordCount",SqlDbType.Int ) //记录总数
+                                         };
+                parameters[0].Value = 35;
+                parameters[1].Value = 9999;
+                parameters[2].Value = "2020-03-31";
+                parameters[3].Value = "2020-04-10";
+                parameters[4].Value = 1;
+                parameters[5].Value = 50;
+                parameters[6].Direction = ParameterDirection.Output;
+                parameters[7].Direction = ParameterDirection.Output;
+                DataTable dt = DapperHelper.RunProcedure_DataTable("UP_OrderPackageAbnormal_Page_Query", parameters, DapperHelper.DBConnection.OrderHelper);
+                int intResult = (int)parameters[6].Value;
+                string strResult = parameters[7].Value.ToString();
+                MessageBox.Show(dt.Rows.Count.ToString()+intResult.ToString()+strResult);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //int result = -1;
+            //string errorMessage = "";
+            //try
+            //{
+            //    IDataParameter[] parameters ={ 
+            //        new SqlParameter("@customerOrderId",SqlDbType.NVarChar),
+            //        new SqlParameter("@Islock",SqlDbType.Int),
+            //        new SqlParameter("@lastUpdateby",SqlDbType.NVarChar,100),
+            //        new SqlParameter("@ErrorMessage",SqlDbType.NVarChar,100 )
+            //    };
+            //    parameters[0].Value = "99888881";
+            //    parameters[1].Value = 1;
+            //    parameters[2].Value = "lwf";
+            //    parameters[3].Direction = ParameterDirection.Output;
+            //    //int rowsAffected = 0;
+            //    //result = DapperHelper.RunProcedure_Return("[UP_OrderPackage_IsSetLock]", parameters, DapperHelper.OrderConnStr, out rowsAffected);
+            //    object resultO = DapperHelper.RunProcedure_Return("[UP_OrderPackage_IsSetLock]", parameters, DapperHelper.OrderConnStr);
+            //    result = Convert.ToInt32(resultO);
+            //    errorMessage = parameters[3].Value.ToString();
+
+            //}
+            //catch (Exception ex)
+            //{
+            //    errorMessage = ex.Message;
+            //}
+            
+
+
+            int result = -1;
+            string errorMessage = "";
+            try
+            {
+                IDataParameter[] parameters ={ 
+                                         new SqlParameter("@customerOrderId",SqlDbType.NVarChar),
+                                         new SqlParameter("@orgId",SqlDbType.Int),
+                                         new SqlParameter("@OrderStatus",SqlDbType.Int),
+                                         new SqlParameter("@lastUpdateby",SqlDbType.NVarChar),
+                                         new SqlParameter("@ErrorMessage",SqlDbType.NVarChar,100 )
+                                         };
+                parameters[0].Value = "99888899";
+                parameters[1].Value = 1;
+                parameters[2].Value = 500;
+                parameters[3].Value = "OMS";
+                parameters[4].Direction = ParameterDirection.Output;
+                int rowsAffected = 0;
+                result = DapperHelper.RunProcedure_Return("[UP_TempOrderStatus_Update]", parameters, DapperHelper.DBConnection.OrderHelper, out rowsAffected);
+                errorMessage = parameters[4].Value.ToString();
+                MessageBox.Show(result.ToString() + errorMessage);
+
+            }
+            catch (Exception ex)
+            {
+                errorMessage =  ex.Message;
+            }
+            MessageBox.Show(result.ToString() + errorMessage);
+
+        }
+    }
+}
